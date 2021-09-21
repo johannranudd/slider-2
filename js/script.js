99ffcb04029da8f70a8323cc7f1e1af7f7c0e238
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-09-21 11:12:05 +0200

    serhg

--- js/script.js
+++ js/script.js
@@ -2,16 +2,64 @@ const slides = document.querySelectorAll(".slide");
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 
-// slides.forEach(function (item) {
-//   console.log(item);
-// });
+const list = [
+  {
+    id: 1,
+    img: "https://github.com/john-smilga/javascript-basic-projects/blob/master/15-slider/setup/img-1.jpeg?raw=true",
+  },
+  {
+    id: 2,
+    img: "https://github.com/john-smilga/javascript-basic-projects/blob/master/15-slider/setup/img-2.jpeg?raw=true",
+  },
+  {
+    id: 3,
+    img: "https://github.com/john-smilga/javascript-basic-projects/blob/master/15-slider/setup/person-1.jpeg?raw=true",
+  },
+];
 
-let counter = 0;
+let index = 0;
+let counter;
+
+window.addEventListener("DOMContentLoaded", function () {
+  counter = 0;
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.left = `${counter * 100}%`;
+    counter++;
+  }
+});
+
+// function createSlider(arg) {
+//   list.map(function (item) {
+//     const slide = document.createElement("div");
+//     slide.classList.add(".slide");
+//     slide.innerHTML = `<img
+//     src="${item.img}"
+//     alt="slider image"
+//   />
+//   <p>${item.id}</p>`;
+//   });
+// }
+// createSlider(slides);
+
+function slide() {
+  slides.forEach(function (item) {
+    item.style.transform = `translateX(-${index * 100}%)`;
+  });
+}
 
 nextBtn.addEventListener("click", function () {
-  counter++;
-  if (counter > slides.length - 1) {
-    counter = 0;
+  index++;
+  if (index > slides.length - 1) {
+    index = 0;
   }
-  console.log(counter);
+  slide();
 });
+
+prevBtn.addEventListener("click", function () {
+  index--;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  slide();
+});
+// console.log(index);
